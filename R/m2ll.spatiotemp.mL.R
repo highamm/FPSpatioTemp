2672f4ce59b9a695babe27fd9aaf397b3a561d0e
Matt Higham <highamm@oregonstate.edu>
2019-10-25 15:30:26 -0400

    Deleted .hello function and fixed initial errors.

--- R/m2ll.spatiotemp.mL.R
+++ R/m2ll.spatiotemp.mL.R
@@ -1,3 +1,23 @@
+#' Covariance Parameter and Mean Estimation Function through Maximum Likelihood.
+#'
+#' The primary purpose of \code{m2ll.spatiotemp.mL.R} is to estimate the spatial
+#' covariance parameters and fixed effects through maximum likelihood.
+#'
+#' @param theta is the parameter vector of (nugget, partialsill, range,
+#' (vector of mean parameters), rho), where rho is the correlation for 
+#' an AR(1) time series
+#' @param zcol is the response vector of counts, with \code{NA} values 
+#' for the missing values for sites that were not observed at a particular
+#' time point.
+#' @param XDesign is the design matrix containing the covariates used to predict animal or plant abundance (including a column of 1's for the intercept) for all
+#' sites at all time points..
+#' @param xcoord is a vector of the x spatial coordinates (in UTM)
+#' @param ycoord is a vector of the y spatial coordinates (in UTM)
+#' @param timepoints is a vector of timepoints for the model
+#' @param CorModel is the geostatistical spatial correlation model to be used. See the \code{corModels} documentation for possible models to use.
+
+#' @return A numeric output of minus 2 times the restricted log likelihood to be minimized by `optim` to obtain spatial parameter estimates.
+
 m2LL.spatiotemp.ML <- function(theta, zcol, XDesign, xcoord, ycoord,
   timepoints, CorModel)
 {
@@ -36,7 +56,7 @@ m2LL.spatiotemp.ML <- function(theta, zcol, XDesign, xcoord, ycoord,
   
   sampindx <- is.na(zcol) == FALSE
   
-  Cmat.nodet <- kronecker(Sigma, Sigmatime)[sampindx, sampindx]
+  Cmat.nodet <- kronecker(Cmat.spatial, Sigmatime)[sampindx, sampindx]
   
   Ci <- mginv(Cmat.nodet)
 
