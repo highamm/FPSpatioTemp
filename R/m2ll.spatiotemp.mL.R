1f71debed53d53366f2e94b569be6d95048fc68c
Matt Higham <highamm@oregonstate.edu>
2019-10-25 15:09:34 -0400

    Push initial functions.

---
+++ R/m2ll.spatiotemp.mL.R
@@ -0,0 +1,52 @@
+m2LL.spatiotemp.ML <- function(theta, zcol, XDesign, xcoord, ycoord,
+  timepoints, CorModel)
+{
+  nspat <- length(zcol) / length(timepoints)
+  p <- length(XDesign[1,])
+  nugget <- as.numeric(exp(theta[1]))
+  parsil <- as.numeric(exp(theta[2]))
+  range <- as.numeric(exp(theta[3]))
+  beta <- matrix(as.numeric(theta[4:(length(theta) - 1)]))
+
+  DM <- matrix(0, nspat, nspat)
+  DM[lower.tri(DM)] <- stats::dist(as.matrix(cbind(xcoord, ycoord)))
+  Dismat <- DM + t(DM)
+
+  if (CorModel == "Exponential") {
+    Sigmat <- parsil * exp(-Dismat / range)
+    Cmat.spatial <- diag(nugget, nrow = nrow(Sigmat)) + Sigmat
+  } ##else if (CorModel == "Gaussian") {
+    ##Sigmat <- parsil * (corModelGaussian(Dismat, range))
+    ##Cmat.nodet <- diag(nugget, nrow = nrow(Sigmat)) + Sigmat
+  ##} else if (CorModel == "Spherical") {
+  ##  Sigmat <- parsil * corModelSpherical(Dismat, range)
+  ##  Cmat.nodet <- diag(nugget, nrow = nrow(Sigmat)) +
+  ##    Sigmat
+  ##}
+  
+  times <- min(timepoints):max(timepoints)
+  ntime <- length(times)
+  H <- abs(outer(times, times, "-")) 
+  
+  Sigmatime <- matrix(0, nrow = ntime, ncol = ntime)
+
+  rhotime <- exp(theta[5]) / (1 + exp(theta[5]))
+  
+  Sigmatime <- 1 * rhotime ^ H 
+  
+  sampindx <- is.na(zcol) == FALSE
+  
+  Cmat.nodet <- kronecker(Sigma, Sigmatime)[sampindx, sampindx]
+  
+  Ci <- mginv(Cmat.nodet)
+
+  zcolsamp <- zcol[sampindx]
+  
+  minus2loglik <- log(det(Cmat.nodet)) +
+    (t(as.matrix(zcolsamp) - as.matrix(XDesign[sampindx, ] %*% beta))) %*%
+    Ci %*%
+    (as.matrix(zcolsamp) - as.matrix(XDesign[sampindx, ] %*% beta)) ##+
+   ## n * log(2 * pi)
+
+  return(as.numeric(minus2loglik))
+}
---
+++ inst/spatiotemporalsim.R
@@ -0,0 +1,112 @@
+## separable models: can separate the spatial and temporal covariance 
+## components so that they are multiplied in the errors
+
+## Suppose that we have a separable model and we have data on a 20 x 20
+## grid over 5 years (for 2000 observations)
+
+library(MASS)
+
+beta0 <- 10
+
+xcoords <- 1:20; ycoords <- 1:20
+allcoords <- expand.grid(xcoords, ycoords)
+distancemat <- as.matrix(dist(allcoords))
+nspat <- nrow(distancemat)
+range <- 10; parsil <- 4
+Sigma <- parsil * exp(-distancemat / range)
+
+ntime <- 5
+times <- 1:ntime
+
+Sigmatime <- matrix(0, nrow = ntime, ncol = ntime)
+H <- abs(outer(times, times, "-")) 
+##assume sigma is 1: we have a separable model
+rhotime <- 0.7; sigmatime <- 1
+
+Sigmatime <- sigmatime * rhotime ^ H 
+Sigmaboth <- kronecker(Sigma, Sigmatime)
+Sigmaboth[1:5, 1:5]
+
+ntotal <- ntime * nspat
+epsilon <- mvrnorm(n = 1, mu = rep(0, ntotal), Sigma = Sigmaboth)
+
+counts <- round(beta0 + epsilon, 0)
+
+## get rid of some of the observations
+nsamp <- 400
+indxkeep <- sample(1:ntotal, nsamp, replace = FALSE)
+
+countswithna <- counts
+countswithna[-indxkeep] <- NA
+
+countssamp <- counts[indxkeep]
+Sigmasamp <- Sigmaboth[indxkeep, indxkeep]
+
+countstemporary <- countswithna[1:((ntime - 1) * nspat)]
+countsallnew_compold <- c(countstemporary[!is.na(countstemporary)],
+  countswithna[((ntime - 1) * nspat + 1): ntotal])
+
+Sigmaallnew_compold <- Sigmaboth[c(countswithna[1:((ntime - 1) * nspat)][!is.na(countswithna[1:((ntime - 1) * nspat)])], countswithna[((ntime - 1) * nspat + 1): ntotal]), 
+  c(countswithna[1:((ntime - 1) * nspat)][!is.na(countswithna[1:((ntime - 1) * nspat)])], countswithna[((ntime - 1) * nspat + 1): ntotal])]
+nrow(Sigmaallnew_compold)
+
+source("m2ll.spatiotemp.ml.R")
+source("mginv.R")
+
+m2LL.spatiotemp.ML(theta = c(1, 1, 1, 2, 1), zcol = countswithna,
+  XDesign = matrix(1, nrow = length(counts)), xcoord = allcoords[ ,1],
+  ycoord = allcoords[ ,2],
+  timepoints = 1:5, CorModel = "Exponential")
+
+parmest <- optim(c(1, 1, 1, 2, 1), m2LL.spatiotemp.ML,
+  zcol = countswithna,
+  XDesign = matrix(1, nrow = length(counts)), xcoord = allcoords[ ,1],
+  ycoord = allcoords[ ,2],
+  timepoints = 1:5, CorModel = "Exponential")
+
+nugget_hat <- exp(parmest$par[1])
+parsil_hat <- exp(parmest$par[2])
+range_hat <- exp(parmest$par[3])
+beta_hat <- parmest$par[4]
+rho_hat <- exp(parmest$par[5]) / (1 + exp(parmest$par[5]))
+
+
+Sigmaspatest <- diag(nugget_hat, nrow = nrow(distancemat)) +
+  parsil_hat * exp(-distancemat / range_hat)
+Sigmatimeest <- rho_hat ^ H 
+Sigmaest <- kronecker(Sigmaspatest, Sigmatimeest)
+
+
+Sigma.ss <- Sigmaest[indxkeep, indxkeep]
+Sigma.us <- Sigmaest[-indxkeep, indxkeep]
+Sigma.su <- t(Sigma.us)
+Sigma.uu <- Sigma[-indxkeep, -indxkeep]
+
+
+Sigma.ssi <- solve(Sigma.ss)
+
+## the generalized least squares regression coefficient estimates
+betahat <- beta_hat
+
+## estimator for the mean vector
+
+Xs <- matrix(1, length(counts[indxkeep]))
+Xu <- matrix(1, length(counts[-indxkeep]))
+
+muhats <- Xs %*% beta_hat
+muhatu <- Xu %*% beta_hat
+
+muhat <- rep(NA, length(counts))
+muhat[indxkeep] <- muhats
+muhat[-indxkeep] <- muhatu
+
+
+
+Cmat <- Sigma.ss %*% as.matrix(Bs) +
+  Sigma.su %*% as.matrix(Bu)
+Dmat <- t(X) %*% matrix(B) - t(Xs) %*% Sigma.ssi %*% Cmat
+Vmat <- solve(t(Xs) %*% Sigma.ssi %*% Xs)
+
+## the predicted values for the sites that were not sampled
+zhatu <- Sigma.us %*% Sigma.ssi %*% (countssamp -
+    muhats) + muhatu
